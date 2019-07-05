d45a3f002bf8f2b24baf5c92df35f243b8770e1e
Geo Pertea <geo.pertea@gmail.com>
2019-07-05 16:02:29 -0400

    adding PrHilightable interface

--- PrPolymorphy/PrCircle.java
+++ PrPolymorphy/PrCircle.java
@@ -1,23 +1,36 @@
 
-public class PrCircle extends PrObj implements PrShowable, PrClickable {
+public class PrCircle extends PrObj implements PrShowable, PrHilightable, PrClickable {
 
 	PrCircle(PrApp parent, float ax, float ay, float asize, int acol) {
 		super(parent, ax, ay, asize, acol);
 	}
 	
+	public boolean contains(float ax, float ay) {
+        return (PrApp.dist(x+size/2, y+size/2, ax, ay)<=size/2);		
+    }
+	
+	public boolean hilight() { //return the "brightened" version of the given color
+   	    if (contains(pr.mouseX, pr.mouseY)) {
+			   pr.strokeWeight((float)1.5);
+			   pr.stroke(0,0,100);
+			   pr.fill(pr.color(pr.hue(col), pr.saturation(col), pr.brightness(col)+10));
+			   return true;
+		   }
+		return false;
+	}
+
 	public void show() {
 	   //  Our custom method which draws this object 
 	   //  on the parent application canvas,  every frame
 	   // We will make use of pr. graphical methods here!
-	   pr.noStroke();
-	   pr.fill(col);
+	   if (!hilight()) {
+	      pr.fill(col);
+	      pr.noStroke();
+	   }
 	   pr.circle(x+size/2, y+size/2, size);
 	}
 	
-	public boolean contains(float ax, float ay) {
-            return (PrApp.dist(x+size/2, y+size/2, ax, ay)<=size/2);		
-	}
-	
+
 	public void mouseLClick() {
 		shiftHue();
 	}
--- PrPolymorphy/PrSquare.java
+++ PrPolymorphy/PrSquare.java
@@ -1,31 +1,39 @@
 
-public class PrSquare extends PrObj implements PrShowable, PrClickable {
+public class PrSquare extends PrObj implements PrShowable, PrHilightable {
 
 	PrSquare(PrApp parent, float ax, float ay, float asize, int acol) {
 		super(parent, ax, ay, asize, acol);
+	}
+	
+	public boolean hilight() { //return the "brightened" version of the given color
+   	    if (contains(pr.mouseX, pr.mouseY)) {
+			   pr.strokeWeight((float)1.5);
+			   pr.stroke(0,0,100);
+			   pr.fill(pr.color(pr.hue(col), pr.saturation(col), pr.brightness(col)+10));
+			   return true;
+		   }
+		return false;
+	}
+
+	public boolean contains(float ax, float ay) {
+		  return (ax>=x && ax<=x+size && ay>=y && ay<=y+size);
 	}
 	
 	public void show() {
 		   //  Our custom method which draws this object 
 		   //  on the parent application canvas,  every frame
 		   // We will make use of pr. graphical methods here!
-		   //mouseOver=contains(pr.mouseX, pr.mouseY); //test for every frame!
-		   //int mcol=col;
-		   //if (mouseOver)
-			//   mcol=pr.color(pr.hue(col), pr.saturation(col), pr.brightness(col)+10);
-		   pr.noStroke();
-		   //if (mousePressedL) pr.stroke(80);
-		   pr.fill(col);
+		   if (!hilight()) {
+			      pr.fill(col);
+			      pr.noStroke();
+		   }
 		   pr.rect(x, y, size, size);  // draw a square
 		 }
 
-	public boolean contains(float ax, float ay) {
-		  return (ax>=x && ax<=x+size && ay>=y && ay<=y+size);
-	}
-	
+	/*
 	public void mouseLClick() {
 		shiftHue();
 	}
 	public void mouseRClick() { }
-
+    */
 }
