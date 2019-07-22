5306117cd40daa6c76724394abd25b7010dea104
Geo Pertea <geo.pertea@gmail.com>
2019-07-22 11:04:28 -0400

    simplify the implementation, only specialize the comparator implementations

--- BubbleSortComparator/src/SelectionSort.java
+++ BubbleSortComparator/src/SelectionSort.java
@@ -1,34 +1,22 @@
 import java.util.*;
 
-class sortByName implements Comparator<Student> {
+class NameComparator implements Comparator<Student> {
 	public int compare(Student a, Student b) {
 		return b.firstName.compareTo(a.firstName);
 	}
 }
 
-class sortByLast implements Comparator<Student> {
+class LastComparator implements Comparator<Student> {
 	public int compare(Student a, Student b) {
 		return b.lastName.compareTo(a.lastName);
 	}
 }
 
-class sortByGrade implements Comparator<Student> {
+class GradeComparator implements Comparator<Student> {
 	public int compare(Student a, Student b) {
 		if (a.grade>b.grade) return -1;
-		else if (a.grade<b.grade) return 1;
-		else return 0;
-	}
-}
-
-class CmpIncreasing implements Comparator<Integer> {
-	public int compare(Integer a, Integer b) {
-		return a.compareTo(b);
-	}
-}
-
-class CmpDecreasing implements Comparator<Integer> {
-	public int compare(Integer a, Integer b) {
-		return -a.compareTo(b);
+		if (a.grade<b.grade) return 1;
+		return 0;
 	}
 }
 
@@ -36,7 +24,7 @@ class CmpDecreasing implements Comparator<Integer> {
 
 public class SelectionSort {
 
-	public static <T> void genSelSort(ArrayList<T> arr,  Comparator cmp) {
+	public static void selectionSort(ArrayList arr,  Comparator cmp) {
 		for (int o = 0; o < arr.size(); o++) { //outer loop
 			int minIdx = o;
 			for (int j=o+1;j<arr.size();j++) {
@@ -45,7 +33,7 @@ public class SelectionSort {
 				}
 			}
 			if (minIdx!=o) {
-				T temp=arr.get(minIdx);
+				Object temp=arr.get(minIdx);
 				arr.set(minIdx, arr.get(o));
 				arr.set(o, temp);
 			}
@@ -54,15 +42,7 @@ public class SelectionSort {
 
 	
 	public static void main(String[] args) {
-		/*
-		ArrayList<Integer> arr=new ArrayList<>(Arrays.asList(12, 7, 3, 1, -1, 2 , 4));
-		System.out.println("Before sorting: "+arr);
-		genBubbleSort(arr, new CmpIncreasing());
-		System.out.println("After sorting increasingly: "+arr);
-		genBubbleSort(arr, new CmpDecreasing());
-		System.out.println("After sorting decreasingly: "+arr);
-		*/
-		ArrayList<Student> students=new ArrayList<Student>();
+		ArrayList students=new ArrayList();
 		students.add(new Student("Darth", "Vader", 'F'));
 		students.add(new Student("Anne", "Frank", 'A'));
 		students.add(new Student("Zoe", "Saldana", 'B'));
@@ -72,11 +52,11 @@ public class SelectionSort {
 		students.add(new Student("Krusty", "Krab", 'E'));
 		students.add(new Student("Donald", "Trump", 'G'));
 		System.out.println("Before sorting: "+students);
-		genSelSort(students, new sortByGrade());
+		selectionSort(students, new GradeComparator());
 		System.out.println("After sorting by grade: "+students);
-		genSelSort(students, new sortByName());
+		selectionSort(students, new NameComparator());
 		System.out.println("After sorting by first name: "+students);
-		genSelSort(students, new sortByLast());
+		selectionSort(students, new LastComparator());
 		System.out.println("After sorting by last name: "+students);
 	}
 }
--- BubbleSortComparator/src/Student.java
+++ BubbleSortComparator/src/Student.java
@@ -1,4 +1,4 @@
-public class Student implements Comparable<Student> {
+public class Student {
 	String firstName;
 	String lastName;
 	char grade;
@@ -6,9 +6,6 @@ public class Student implements Comparable<Student> {
 		firstName=first;
 		lastName=last;
 		grade=gr;
-	}
-	public int compareTo(Student other) {
-		return Character.compare(grade, other.grade);
 	}
 	public String toString() {
 		return firstName + " "+lastName + " ["+ Character.toString(grade) +"]";
